f254e3f2168dd8b9358fbf69b8e695faa1ee3a90
jbarr191 <nokama99@gmail.com>
2018-01-26 16:49:43 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE>
+<html>
+	<head>
+		<title>My Online Shop</title>
+
+		<link rel="stylesheet" href="styles/style.css" media="all" />
+	</head>
+<body>
+
+	<!--Main Container starts here-->
+	<div class="main_wrapper">
+
+		<div class="header_wrapper">
+
+			<img id="logo" src="images/logo.jpg" width="375" height="175" />
+			<img id="banner" src="images/banner.jpg" width"800" height="175" />
+
+
+		</div>
+
+	<div class="menubar">
+
+		<ul id="menu">
+			<li><a href="">Home</a></li>
+			<li><a href="">All Products</a></li>
+			<li><a href="">My Account</a></li>
+			<li><a href="">Log In</a></li>
+			<li><a href="">Shopping Cart</a></li>
+			<li><a href="">Contact Us</a></li>
+		</ul>
+
+	</div>
+
+		<div class="content_wrapper">
+
+			<div id="sidebar">sidebar</div>
+
+			<div id="content_area">Content Area</div>
+
+		</div>
+
+
+		<div id="footer">footer</div>
+
+	</div>
+	<!--Main Container ends here-->
+
+</body>
+</html>
