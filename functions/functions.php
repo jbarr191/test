fbfee81206a89e33b3ae89dda6a28311caefd0bf
jbarr191 <nokama99@gmail.com>
2018-02-10 06:07:52 -0500

    functions.php added
    Added the functions.php file, with some very early attempts at php-sql coding

---
+++ functions/functions.php
@@ -0,0 +1,12 @@
+<?php
+
+$con = mysqli("localhost","root","","test");
+
+function getAccount($email, $password){
+
+	global $con;
+
+	$get_acc() = "select * from account";
+
+	$run_acc() = mysqli_query($con, $get_acc);
+}
