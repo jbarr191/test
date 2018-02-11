e396f7efd2c1c5af138e799da241d0802567ef46
ealfaras <ealfa009@fiu.edu>
2018-02-11 18:31:43 -0500

    Add product(book)
    Add book page is working. When you enter data into the fields, the database updates with the book and the location of the image.
    - some of the data types need to be changed for some columns.

--- admin_area/insert_products.php
+++ admin_area/insert_products.php
@@ -11,7 +11,7 @@ include("includes/db.php");
 	
 <body bgcolor="skyblue">
 	<!-- start of insert form -->
-	<form action= "insert_product.php" method = "post" enctype = "multipart/form-data">
+	<form action= "insert_products.php" method = "post" enctype = "multipart/form-data">
 		<!-- start table  -->
 		<table align ="center" width = "750" border="2" bgcolor="orange">
 			<!-- insert into table  -->
@@ -24,24 +24,24 @@ include("includes/db.php");
 				<!-- column 1 -->
 				<td align = "right"><b>Book Title:</b></td>
 				<!-- column 2 -->
-				<td><input type="text" name="Book_title" /></td>
+				<td><input type="text" name="product_title" size="60"/></td>
 			</tr>
 			<tr>
 				<td align = "right"><b>Book Author:</b></td>
-				<td><input type="text" name="product_title" /></td>
+				<td><input type="text" name="product_author"/></td>
 			</tr>
 			<tr>
 				<td align = "right"><b>Book Description:</b></td>
-				<td><input type="text" name="product_title" /></td>
+				<td><textarea name = "product_desc" cols ="20" rows="10"></textarea></td>
 			</tr>
 			<tr>
 				<td align = "right"><b>Author Biography:</b></td>
-				<td><input type="text" name="product_title" /></td>
+				<td><textarea name = "product_bio" cols ="20" rows="10"></textarea></td>
 			</tr>
 			<tr>
 				<td align = "right"><b>Book Genre:</b></td>
 				<td>
-					<select name = "book_genre">
+					<select name = "product_genre">
 						
 						<option>Select a Genre</option>
 						<!--query to get genres from database -->
@@ -69,15 +69,15 @@ include("includes/db.php");
 			</tr>
 			<tr>
 				<td align = "right"><b>Book Publisher:</b></td>
-				<td><input type="text" name="product_title" /></td>
+				<td><input type="text" name="product_pub" /></td>
 			</tr>
 			<tr>
 				<td align = "right"><b>Book Release Date:</b></td>
-				<td><input type="text" name="product_title" /></td>
+				<td><input type="text" name="product_release" /></td>
 			</tr>
 			<tr>
 				<td align = "right"><b>Book Cover:</b></td>
-				<td><input type="text" name="product_title" /></td>
+				<td><input type="file" name="product_image" /></td>
 			</tr>
 			<!--insert button -->
 			<tr align="right">
@@ -91,14 +91,42 @@ include("includes/db.php");
 	
 	</form>
 
-
-
-
-
-
-
-
-
 </body>
 
 </html>
+
+<?php
+	//if something is submitted insert using post(), then execute
+	if(isset($_POST['insert_post']))
+	{
+		//get text data from fields
+		$product_title= $_POST['product_title'];
+		$product_author= $_POST['product_author'];
+		$product_desc= $_POST['product_desc'];
+		$product_bio= $_POST['product_bio'];
+		$product_genre= $_POST['product_genre'];
+		$product_pub= $_POST['product_pub'];
+		$product_release= $_POST['product_release'];
+		
+		//getting the image from the field
+		$product_image = $_FILES['product_image']['name'];
+		$product_image_tmp = $_FILES['product_image']['tmp_name'];
+		
+		//used to upload product_image into product_images folder
+		move_uploaded_file($product_image_tmp, "product_images/$product_image");
+		
+		
+		$insert_products = "insert into products(product_title, product_image, product_author, product_desc, product_bio, product_genre, product_pub, product_release) 
+											values('$product_title','$product_image','$product_author','$product_desc','$product_bio','$product_genre','$product_pub','$product_release')";
+		$insert_pro = mysqli_query($con, $insert_products);
+		if($insert_pro)
+		{
+			echo "<script>alert('Product has been inserted!')</script>";
+			//refresh page to avoid duplicate data
+			echo "<script>window.open('insert_products.php','_self')</script>";
+		}
+	}
+
+
+
+?>
