de9c163cd20b17044474f3ea42ee2c891def4736
calon043 <calon043@fiu.edu>
2018-02-16 22:59:10 -0500

    additions to comments

--- functions/functions.php
+++ functions/functions.php
@@ -120,10 +120,13 @@ function getPro(){
 					
 					<a href='index.php?add_cart=$pro_id'><button style='float:right'>Add to Cart</button></a>
 					
-					<a href ='comments.php' style = 'float:right'>Comments</a>
+					<a href ='comments.php?pro_id=$pro_id' style = 'float:right'>Comments</a>
 				</div>			
 		";	
 	}
 }
+
+
+
 
 ?>
