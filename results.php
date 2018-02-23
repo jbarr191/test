56e507bd10d12927829416e52a8d3279231b9049
megakirbyk <35631685+megakirbyk@users.noreply.github.com>
2018-02-23 17:12:57 -0500

    Expanded search values
    -Added the ablility to search by author, price, publisher, and release date
    -If there are no matches in the search results, the page will now display a message that nothing has been found.
    -Still need to make the page display a message to search if the user does not enter anything in the search bar and clicks search

--- results.php
+++ results.php
@@ -79,11 +79,27 @@ include("functions/functions.php");
 						
 						$search_query = $_GET['user_query'];
 						
-						$get_pro = "select * from products where product_title like '%$search_query%'";
+						$get_pro = "select * from products where product_title like '%$search_query%'
+																or product_author like '%$search_query%'
+																or product_price like '%$search_query%'
+																or product_release like '%$search_query%'
+																or product_pub like '%$search_query%'";
 						
 						$run_pro = mysqli_query($con, $get_pro);
 						
-						while($row_pro = mysqli_fetch_array($run_pro)){
+						$count_pro = mysqli_num_rows($run_pro);
+						
+						if($count_pro == 0){
+							echo "<h2 style='padding:20px;'>No search results found!</h2>";
+							}
+						
+						//elseif($user_query = ""){
+						//		echo "<h2 style='padding:20px;'>Try entering something in the search bar!</h2>";
+						//	}
+								
+						else 
+						
+							while($row_pro = mysqli_fetch_array($run_pro)){
 							
 							$pro_id = $row_pro['product_id'];
 							$pro_title = $row_pro['product_title'];
@@ -95,22 +111,20 @@ include("functions/functions.php");
 							$pro_gen = $row_pro['product_genre'];
 							$pro_release = $row_pro['product_release'];
 							
-							echo "
-							<div id='single_product'>
+							
+								echo "
+									<div id='single_product'>
 					
-							<h3>$pro_title</h3>
+									<h3>$pro_title</h3>
 					
-							<img src='admin_area/product_images/$pro_image' width='180' height='180' />
+									<img src='admin_area/product_images/$pro_image' width='180' height='180' />
 					
-							<p><b> Price: $ $pro_price </b></p>
+									<p><b> Price: $ $pro_price </b></p>
 					
-					
-							<a href='index.php?add_cart=$pro_id'><button style='float:right'>Add to Cart</button></a>
+									<a href='details.php?pro_id=$pro_id' style='float:left;'>Details</a>
+									<a href='index.php?add_cart=$pro_id'><button style='float:right'>Add to Cart</button></a>
 				
-						</div>
-		
-		
-						";
+									</div>";
 	
 						}
 					}
