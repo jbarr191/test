b65a5753b5bf8da81c68df20ccfd28a9e48c1978
ealfaras <ealfa009@fiu.edu>
2018-02-26 20:03:32 -0500

    empty search error
    if the user search is empty they will receive no results and an error msg

--- results.php
+++ results.php
@@ -79,53 +79,62 @@ include("functions/functions.php");
 						
 						$search_query = $_GET['user_query'];
 						
-						$get_pro = "select * from products where product_title like '%$search_query%'
-																or product_author like '%$search_query%'
-																or product_price like '%$search_query%'
-																or product_release like '%$search_query%'
-																or product_pub like '%$search_query%'";
-						
-						$run_pro = mysqli_query($con, $get_pro);
-						
-						$count_pro = mysqli_num_rows($run_pro);
-						
-						if($count_pro == 0){
+						if($search_query == 0)
+						{
 							echo "<h2 style='padding:20px;'>No search results found!</h2>";
-							}
+						}
+						else
+						{
 						
-						//elseif($user_query = ""){
-						//		echo "<h2 style='padding:20px;'>Try entering something in the search bar!</h2>";
-						//	}
+							$get_pro = "select * from products where product_title like '%$search_query%'
+																	or product_author like '%$search_query%'
+																	or product_price like '%$search_query%'
+																	or product_release like '%$search_query%'
+																	or product_pub like '%$search_query%'";
+							
+							$run_pro = mysqli_query($con, $get_pro);
+							
+							$count_pro = mysqli_num_rows($run_pro);
+							
+							if($count_pro == 0){
+								echo "<h2 style='padding:20px;'>No search results found!</h2>";
+								}
+							
+							//elseif($user_query = ""){
+							//		echo "<h2 style='padding:20px;'>Try entering something in the search bar!</h2>";
+							//	}
+									
+							else 
+							
+								while($row_pro = mysqli_fetch_array($run_pro))
+								{
 								
-						else 
+								$pro_id = $row_pro['product_id'];
+								$pro_title = $row_pro['product_title'];
+								$pro_image = $row_pro['product_image'];
+								$pro_author = $row_pro['product_author'];
+								$pro_desc= $row_pro['product_desc'];
+								$pro_price = $row_pro['product_price'];
+								$pro_bio = $row_pro['product_bio'];
+								$pro_gen = $row_pro['product_genre'];
+								$pro_release = $row_pro['product_release'];
+								
+								
+									echo "
+										<div id='single_product'>
 						
-							while($row_pro = mysqli_fetch_array($run_pro)){
-							
-							$pro_id = $row_pro['product_id'];
-							$pro_title = $row_pro['product_title'];
-							$pro_image = $row_pro['product_image'];
-							$pro_author = $row_pro['product_author'];
-							$pro_desc= $row_pro['product_desc'];
-							$pro_price = $row_pro['product_price'];
-							$pro_bio = $row_pro['product_bio'];
-							$pro_gen = $row_pro['product_genre'];
-							$pro_release = $row_pro['product_release'];
-							
-							
-								echo "
-									<div id='single_product'>
+										<h3>$pro_title</h3>
+						
+										<img src='admin_area/product_images/$pro_image' width='180' height='180' />
+						
+										<p><b> Price: $ $pro_price </b></p>
+						
+										<a href='details.php?pro_id=$pro_id' style='float:left;'>Details</a>
+										<a href='index.php?add_cart=$pro_id'><button style='float:right'>Add to Cart</button></a>
 					
-									<h3>$pro_title</h3>
-					
-									<img src='admin_area/product_images/$pro_image' width='180' height='180' />
-					
-									<p><b> Price: $ $pro_price </b></p>
-					
-									<a href='details.php?pro_id=$pro_id' style='float:left;'>Details</a>
-									<a href='index.php?add_cart=$pro_id'><button style='float:right'>Add to Cart</button></a>
-				
-									</div>";
-	
+										</div>";
+		
+								}	
 						}
 					}
 							
