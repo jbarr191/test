42158e2b381a5300a9c201673a8b60ed11ef0766
jess <b.jessiel@yahoo.com>
2018-03-23 13:29:22 -0400

    Links across .php files
    Files should now be properly linked to one another

--- cart.php
+++ cart.php
@@ -30,10 +30,20 @@ include("functions/functions.php");
 
 		<ul id="menu">
 			<li><a href="index.php">Home</a></li>
-			<li><a href="">All Products</a></li>
-			<li><a href="">My Account</a></li>
-			<li><a href="customer_login.php">Log In</a></li>
-			<li><a href="">Shopping Cart</a></li>
+			<li><a href="index.php">All Products</a></li>
+			
+			<?php
+			if (isset($_SESSION['customer_email'])){
+
+				echo "<li><a href='customer/customer_account.php'>My Account</a></li>";
+
+			} else {
+
+				echo "<li><a href='customer_login.php'>Log In</a></li>";
+				echo "<li><a href='customer_register.php'>Register</a></li>";
+			}
+			?>
+			<li><a href="cart.php">Shopping Cart</a></li>
 			<li><a href="">Contact Us</a></li>
 		</ul>
 
