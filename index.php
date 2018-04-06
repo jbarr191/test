e052c054a8b91108b540ea6dccfdb55bb56a793c
jbarr191 <nokama99@gmail.com>
2018-04-06 06:33:50 -0400

    Account page is pretty much finished
    Only missing some aligning of the user image, and some other misc. touch-ups.

--- index.php
+++ index.php
@@ -28,12 +28,12 @@ $con = mysqli_connect("localhost","root","","onlinebookstore");
 
 	<!-- Sidebar/menu -->
 	<nav class="w3-sidebar w3-bar-block w3-white w3-collapse w3-top" style="z-index:3;width:250px" id="mySidebar">
-	  <div class="w3-container w3-display-container w3-padding-16">
-	    <i onclick="w3_close()" class="fa fa-remove w3-hide-large w3-button w3-display-topright"></i>
-		 <a href="index.php">
-		 	<img id="logo" src="images/logo.jpg" width="240" height="120" />
-		</a>
-	  </div>
+		<div class="w3-container w3-display-container w3-padding-16">
+			<i onclick="w3_close()" class="fa fa-remove w3-hide-large w3-button w3-display-topright"></i>
+		 	<a href="index.php">
+		 		<img id="logo" src="images/logo.jpg" width="240" height="120" />
+			</a>
+	  	</div>
 	  <div class="w3-padding-64 w3-large w3-text-grey" style="font-weight:bold">
 	    <a href="#" class="w3-bar-item w3-button">Best-Sellers</a>
 	    <a href="#" class="w3-bar-item w3-button">Top-Rated</a>
