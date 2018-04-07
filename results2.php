4282ff44a048e62b027ad7b79d6b953c0613fc8d
megakirbyk <35631685+megakirbyk@users.noreply.github.com>
2018-04-06 21:16:41 -0400

    Changing the look to the new style
    Changing the look to the new layout, results2 and 3 are just extra copies

--- results2.php
+++ results2.php
@@ -9,110 +9,64 @@ include("functions/functions.php");
 <html>
 	<head>
 		<title>My Online Shop</title>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
-		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-		<!--<link rel="stylesheet" href="styles/style.css" media="all" />-->
-		<style>
-			.w3-sidebar a {font-family: "Roboto", sans-serif}
-			body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
-		</style>
-	</head>
-	
-<body class="w3-content" style="max-width:1200px">
 
-<!-- Sidebar/menu -->
-	<nav class="w3-sidebar w3-bar-block w3-white w3-collapse w3-top" style="z-index:3;width:250px" id="mySidebar">
-	  <div class="w3-container w3-display-container w3-padding-16">
-	    <i onclick="w3_close()" class="fa fa-remove w3-hide-large w3-button w3-display-topright"></i>
-		 <a href="index.php">
-		 	<img id="logo" src="images/logo.jpg" width="240" height="120" />
-		</a>
-	  </div>
-	  
-	  <div class="w3-padding-64 w3-large w3-text-grey" style="font-weight:bold">
-	    <a href="#" class="w3-bar-item w3-button">Best-Sellers</a>
-	    <a href="#" class="w3-bar-item w3-button">Top-Rated</a>
-	    <a onclick="myAccFunc()" href="javascript:void(0)" class="w3-button w3-block w3-white w3-left-align" id="myBtn">
-	      Genres <i class="fa fa-caret-down"></i>
-	    </a>
-	    <div id="demoAcc" class="w3-bar-block w3-hide w3-padding-large w3-medium">
-	      <a href="#" class="w3-bar-item w3-button">Sci-fi</a>
-	      <a href="#" class="w3-bar-item w3-button">Fiction</a>
-	      <a href="#" class="w3-bar-item w3-button">Genre 3</a>
-	    </div>
-	  </div>
-	  <a href="#footer" class="w3-bar-item w3-button w3-padding">Contact Us</a>
-	</nav>
+		<link rel="stylesheet" href="styles/style.css" media="all" />
+	</head>
+<body>
 
 	<!--Main Container starts here-->
-	<!-- Overlay effect when opening sidebar on small screens -->
-	<div class="w3-overlay w3-hide-large" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
-
-	<!-- !PAGE CONTENT! -->
-	<div class="w3-main" style="margin-left:250px">
-
-	  <!-- Push down content on small screens -->
-	  <div class="w3-hide-large" style="margin-top:83px"></div>
-
-	  <!-- Top header -->
-	  <header class="w3-container w3-xlarge">
-	    <p class="w3-left" style="padding:8px; font-size:20px"><a href="index.php">Home</a></p>
-		 <p class="w3-left" style="padding:8px; font-size:20px">All Products</p>
-		 <?php
-		 if (isset($_SESSION['customer_email'])){
-
-			 echo "<p class='w3-left' style='padding:8px; font-size:20px'><a href='customer/customer_account.php'>My Account</a></p>";
-
-		 } else {
-
-			 echo "<p class='w3-left' style='padding:8px; font-size:20px'><a href='customer_login.php'>Log In</a></p>";
-			 echo "<p class='w3-left' style='padding:8px; font-size:20px'><a href='customer_register.php'>Register</a></p>";
-		 }
-		 ?>
-		 <p class="w3-left" style="padding:8px; font-size:20px"><a href="cart.php">Shopping Cart</a></p>
-	    <p class="w3-right">
-			 <div id="form" style="line-height:20px; padding-top:24px; float:right">
-	 			<form method="get" action="results.php" enctype="multipart/form-data">
-	 				<input type="text" name="user_query" placeholder="Search for stuff" style="width:200" />
-	 				<input type="submit" name="search" value="Search" />
-	 			</form>
-	 		</div>
-	      <!--<i class="fa fa-search"></i>-->
-	    </p>
-	  </header>
-
-
-		<!--
 	<div class="main_wrapper">
 
--->
+		<div class="header_wrapper">
+
+			<img id="logo" src="images/logo.jpg" width="375" height="175" />
+			<img id="banner" src="images/banner.jpg" width"800" height="175" />
+
+
+		</div>
+
+	<div class="menubar">
+
+		<ul id="menu">
+			<li><a href="index.php">Home</a></li>
+			<li><a href="">All Products</a></li>
+			<li><a href="">My Account</a></li>
+			<li><a href="customer_login.php">Log In</a></li>
+			<li><a href="">Shopping Cart</a></li>
+			<li><a href="">Contact Us</a></li>
+		</ul>
+
+		<div id="form">
+			<form method="get" action="results.php" enctype="multipart/form-data">
+				<input type="text" name="user_query" placeholder="Search for stuff" />
+				<input type="submit" name="search" value="Search" />
+			</form>
+		</div>
+
+	</div>
+
 		<!--content_wrapper starts here-->
-<!--
 		<div class="content_wrapper">
 
 			<div id ="sidebar">
-
+			
 				<div id="sidebar_title">Genres</div>
-
+				
 				<ul id="gens">
-
-					<?php getGens(); ?>
-
+				
+					<?php getGens(); ?>	
+					
 				</ul>
 
-			</div>
-
+			</div>	
+		
 			<div id="content_area">
-
+			
 				<?php cart(); ?>
-
+				
 				<div id="shopping_cart">
-
-					<span style="float:right; font-size:18px; padding:5px; line-height:40px;">
+			
+						<span style="float:right; font-size:18px; padding:5px; line-height:40px;">
 
 					<?php
 					if (isset($_SESSION['customer_email'])){
@@ -144,20 +98,8 @@ include("functions/functions.php");
 
 					?>
 					</span>
-
+				
 				</div>
-
-				<div id="products_box">
-
-					<?php getPro(); ?>
-
-				</div>
-
-			</div>
-
-		</div>
--->
-		<!--content_wrapper ends here-->
 				
 				<div id="sort_select">
 					<form method="get" action="results.php" enctype="multipart/form-data">
@@ -243,21 +185,18 @@ include("functions/functions.php");
 								
 									echo "
 										
-										<div class='w3-col l3 s6'>
-			<div class='w3-container'>
-				<div class='w3-display-container'>
-					<img src='admin_area/product_images/<?php echo $pro_image; ?>' style='width:100%'>
-					<span class='w3-tag w3-display-topleft'>New</span>
-					<div class='w3-display-middle w3-display-hover'>
-						<a href='index.php?add_cart=<?php echo $pro_id; ?>'><button class='w3-button w3-black'>Buy now <i class='fa fa-shopping-cart'></i></button>
-					</div>
-				</div>
-				<p><?php echo $pro_title; ?><br><b>$<?php echo $pro_price; ?></b></p>
-			</div>
-
-			
-	    </div>
-		";
+										<div id='single_product'>
+						
+											<h3>$pro_title</h3>
+						
+											<img src='admin_area/product_images/$pro_image' width='180' height='180' />
+						
+											<p><b> Price: $ $pro_price </b></p>
+						
+											<a href='details.php?pro_id=$pro_id' style='float:left;'>Details</a>
+											<a href='index.php?add_cart=$pro_id'><button style='float:right'>Add to Cart</button></a>
+					
+										</div>";
 		
 								}	
 						}
@@ -276,7 +215,12 @@ include("functions/functions.php");
 							
 						else
 						{
-							
+							//$get_pro = "select * from products where product_title like '%$search_query%'
+																	//or product_author like '%$search_query%'
+																	//or product_price like '%$search_query%'
+																	//or product_release like '%$search_query%'
+																	//or product_pub like '%$search_query%'
+																	//order by product_title asc";
 							
 							if($_GET['refine_search'] == 1)
 							{
@@ -481,43 +425,23 @@ include("functions/functions.php");
 							
 					?>
 				
-				
+				</div>
+			
+			</div>
+
+		</div>
 		<!--content_wrapper ends here-->
 
 
-		<div class="w3-black w3-center w3-padding-24">&copy; 2018 by Software Engineering TEAM 1</div>
-	</div>
+		<div id="footer">
+		
+		<h2 style = "text-align:center; padding-top:30px;">&copy; 2018
+		by software engineering TEAM 1</h2>
+		
+		</div>
 
 	</div>
 	<!--Main Container ends here-->
-	
-	<script>
-		// Accordion
-		function myAccFunc() {
-		    var x = document.getElementById("demoAcc");
-		    if (x.className.indexOf("w3-show") == -1) {
-		        x.className += " w3-show";
-		    } else {
-		        x.className = x.className.replace(" w3-show", "");
-		    }
-		}
-
-		// Click on the "Jeans" link on page load to open the accordion for demo purposes
-		document.getElementById("myBtn").click();
-
-		// Script to open and close sidebar
-
-		function w3_close() {
-		    document.getElementById("mySidebar").style.display = "none";
-		    document.getElementById("myOverlay").style.display = "none";
-		}
-
-		function w3_open() {
-		    document.getElementById("mySidebar").style.display = "block";
-		    document.getElementById("myOverlay").style.display = "block";
-		}
-
-	</script>
 
 </body>
 </html>
