b49bf827ac615f4a47511d9fa8132dcacf748097
calon043 <calon043@fiu.edu>
2018-04-20 22:54:14 -0400

    committing book search sort again for some reason it disappeared

--- results.php
+++ results.php
@@ -326,7 +326,10 @@ include("functions/functions.php");
 								{
 									$search_cat = "product_release";
 								}
-
+								elseif($_GET['refine_search'] == 6)
+								{
+									$search_cat = "ratings";
+								}
 								$pageoffset = ($_GET['page']-1) * 10;
 								$get_pro = "select * from products where product_title like '%$search_query%'
 																	or product_author like '%$search_query%'
@@ -429,6 +432,10 @@ include("functions/functions.php");
 								elseif($_GET['refine_search'] == 5)
 								{
 									$search_cat = "product_release";
+								}
+								elseif($_GET['refine_search'] == 6)
+								{
+									$search_cat = "ratings";
 								}
 
 								$pageoffset = ($_GET['page']-1) * 10;
